0da759b021544039565d2bbf5ecf6c0705cade87
umeshkedimi <umesh836@gmail.com>
2025-07-08 07:20:10 +0530

    Added first go code

---
+++ cmd/tutorial_1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("This is a simple Go program.")
+	fmt.Println("Let's learn Go together!")	
+	fmt.Println("Happy coding!")
+}
