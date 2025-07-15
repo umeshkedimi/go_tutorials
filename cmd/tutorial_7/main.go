e771e662231f981498c89bf994e6767508e29abe
umeshkedimi <umesh836@gmail.com>
2025-07-15 18:48:05 +0530

    Added info related to struct and interfaces

--- cmd/tutorial_7/main.go
+++ cmd/tutorial_7/main.go
@@ -11,24 +11,36 @@ func (e gasEngine) getRange() uint8 {
 	return e.mpg * e.gallons
 }
 
-func canMakeIt(e gasEngine, distance uint8) bool {
-	return e.getRange() >= distance
+type electricEngine struct {
+	kwh uint8
+	charge uint8
 }
 
-func main() {
-	var myEngine gasEngine = gasEngine{22, 3}
-	fmt.Println(myEngine.mpg, myEngine.gallons)
-	fmt.Println("Range of myEngine:", myEngine.getRange())
-	if canMakeIt(myEngine, 80) {
+func (e electricEngine) getRange() uint8 {
+	return e.kwh * e.charge
+}
+
+type engine interface {
+	getRange() uint8
+}
+
+
+func canMakeIt(e engine, miles uint8) {
+	if miles <= e.getRange() {
 		fmt.Println("We can make it!")
 	} else {
 		fmt.Println("We cannot make it!")
 	}
+}
 
+func main() {
+	var myEngine gasEngine = gasEngine{22, 5}
+	fmt.Println(myEngine.mpg, myEngine.gallons)
+	fmt.Println("Range of myEngine:", myEngine.getRange())
+	canMakeIt(myEngine, 60)
 
-	var myEngine2 = struct {
-		mpg uint8
-		gallons uint8
-	}{23, 24}	
-	fmt.Println(myEngine2.mpg, myEngine2.gallons)
+	var myEngine3 = electricEngine{22, 4}
+	fmt.Println(myEngine3.kwh, myEngine3.charge)
+	fmt.Println("Range of myEngine3:", myEngine3.getRange())
+	canMakeIt(myEngine3, 60)
 }
