702fa5c2025b367cb7fb8ae9abb71ad6ef1164e3
umeshkedimi <umesh836@gmail.com>
2025-07-15 17:56:20 +0530

    Added method to struct

--- cmd/tutorial_7/main.go
+++ cmd/tutorial_7/main.go
@@ -7,7 +7,28 @@ type gasEngine struct {
 	gallons uint8
 }
 
+func (e gasEngine) getRange() uint8 {
+	return e.mpg * e.gallons
+}
+
+func canMakeIt(e gasEngine, distance uint8) bool {
+	return e.getRange() >= distance
+}
+
 func main() {
-	var myEngine gasEngine
+	var myEngine gasEngine = gasEngine{22, 3}
 	fmt.Println(myEngine.mpg, myEngine.gallons)
+	fmt.Println("Range of myEngine:", myEngine.getRange())
+	if canMakeIt(myEngine, 80) {
+		fmt.Println("We can make it!")
+	} else {
+		fmt.Println("We cannot make it!")
+	}
+
+
+	var myEngine2 = struct {
+		mpg uint8
+		gallons uint8
+	}{23, 24}	
+	fmt.Println(myEngine2.mpg, myEngine2.gallons)
 }
