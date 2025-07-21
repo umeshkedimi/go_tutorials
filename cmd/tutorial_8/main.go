446744a34d7283f139de66cc56aacc4c18fabbc3
umeshkedimi <umesh836@gmail.com>
2025-07-21 16:06:09 +0530

    pointers wrt to slices

--- cmd/tutorial_8/main.go
+++ cmd/tutorial_8/main.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main()  {
-	var p *int32 = new(int32)
-	var i int32
-	fmt.Printf("The value p points to is: %v", *p)
-	fmt.Printf("\nThe value of i is: %v", i)
-	p = &i
-	*p = 42
-	fmt.Printf("\nThe value p points to is now: %v", *p)
-	fmt.Printf("\nThe value of i is now: %v", i)
-	
+	var slice = []int32{1, 2, 3, 4, 5}
+	var sliceCopy = slice
+	sliceCopy[0] = 10
+	fmt.Println("Original slice:", slice)
+	fmt.Println("Modified copy:", sliceCopy)
+	fmt.Println("Length of original slice:", len(slice))
+	fmt.Println("Capacity of original slice:", cap(slice))
+	fmt.Println("Length of copy slice:", len(sliceCopy))
+	fmt.Println("Capacity of copy slice:", cap(sliceCopy))
+	fmt.Println("Address of original slice:", &slice)
+	fmt.Println("Address of copy slice:", &sliceCopy)	
 }
