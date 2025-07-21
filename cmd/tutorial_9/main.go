975e205da45d4037efadfedcd9bd63d93893ff68
umeshkedimi <umesh836@gmail.com>
2025-07-21 20:07:26 +0530

    Added code to check performance

--- cmd/tutorial_9/main.go
+++ cmd/tutorial_9/main.go
@@ -6,41 +6,27 @@ import (
 	"sync"
 )
 
-var m = sync.RWMutex{}
+
 var wg = sync.WaitGroup{}
 var dbData =[]string{"data1", "data2", "data3", "data4", "data5"}
-var results = []string{}
+
 
 func main() {
 	t0 := time.Now()
-	for i:=0; i < len(dbData); i++ {
+	for i:=0; i < 1000; i++ {
 		wg.Add(1)
 		go dbCall(i)
 	}
 	wg.Wait()
 	fmt.Printf("Total time taken: %v\n", time.Since(t0))
-	fmt.Println("Results:", results)
+	// fmt.Println("Results:", results)
 }
 
 func dbCall(i int) {
 	// Simulate a database call
 	var delay float32 = 2000
 	time.Sleep(time.Duration(delay) * time.Millisecond)
-	fmt.Println("The result from the database is:", dbData[i])
-	save(dbData[i])
-	log()
+	// fmt.Println("The result from the database is:", dbData[i])
 	wg.Done()
 }
 
-func save(result string) {
-	m.Lock()
-	results = append(results, result)
-	m.Unlock()
-	fmt.Println("Saved result:", results)
-}
-
-func log() {
-	m.RLock()
-	fmt.Printf("\nThe current results are: %v\n", results)
-	m.RUnlock()
-}
