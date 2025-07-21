1624d723b714b7392dab251b7f9ae96b02813a3d
umeshkedimi <umesh836@gmail.com>
2025-07-21 20:15:20 +0530

    tested peformance with count

--- cmd/tutorial_9/main.go
+++ cmd/tutorial_9/main.go
@@ -15,18 +15,18 @@ func main() {
 	t0 := time.Now()
 	for i:=0; i < 1000; i++ {
 		wg.Add(1)
-		go dbCall(i)
+		go count()
 	}
 	wg.Wait()
 	fmt.Printf("Total time taken: %v\n", time.Since(t0))
 	// fmt.Println("Results:", results)
 }
 
-func dbCall(i int) {
-	// Simulate a database call
-	var delay float32 = 2000
-	time.Sleep(time.Duration(delay) * time.Millisecond)
-	// fmt.Println("The result from the database is:", dbData[i])
+func count() {
+	var res int
+	for i:= 0; i < 100000000; i++ {
+		res += i
+	}
 	wg.Done()
 }
 
